0c5b3c72f00d254a3e0836b10a3da8f9e2114490
Nam Pham <pknam25@gmail.com>
2022-02-14 20:36:18 +0800

    Added destructuring and shorthand syntax sections

--- src/app/es6-syntax/es6-syntax.component.ts
+++ src/app/es6-syntax/es6-syntax.component.ts
@@ -111,6 +111,274 @@ export class ES6SyntaxComponent implements OnInit {
 
   console.log(concatObj)  // { year: 2021, month: 1, asset: 'AC', product: 'L' }
   `;
+
+  destructuringOperatorFirstExample: string = `
+  const arrayA = [1, 2, 3]
+  const [first, second, last] = arrayA
+
+  console.log(first) // 1
+  console.log(second) // 2
+  console.log(last)  // 3
+  
+  const objA = {
+    year: 2021, 
+    month: 1, 
+    day: 31
+  }
+  const { year, month, day } = objA
+
+  console.log(year) // 2021
+  console.log(month) // 1
+  console.log(day)  // 31
+  `;
+
+  destructuringOperatorArray: string = `
+  const arrayA = [1, 2, 3, 4, 5]
+  const [one, two, ...rest] = arrayA
+
+  console.log(one)  // 1
+  console.log(two)  // 2
+  console.log(rest) // [3, 4, 5]
+
+  // The above is equivalent to the following more 'traditional' way
+  const one = arrayA[0]
+  const two = arrayA[1]
+  const rest = arrayA.slice(2)
+
+  
+  // Also, with the destructuring operator, we can easily swap values of 2 variables like this:
+  let a = 10
+  let b = 20
+  [a, b] = [b, a]   // Now a = 20 and b = 10!
+
+  console.log(a)  // 20
+  console.log(b)  // 10
+
+
+  // The above is equivalent to this more usual way
+  let temp = a
+  a = b
+  b = temp
+  `;
+
+  destructuringOperatorObject: string = `
+  const originalObj = {
+    year: 2021,
+    month: 1,
+    day: 31,
+    tonnes: 1000,
+    asset: 'AC',
+    product: 'F',
+    destinationAsset: 'MAC_SY1'
+  }
+
+  const asset = originalObj.asset
+  const product = originalObj.product
+  //.... blah... blah... Too long, didn't write!
+
+
+  const {
+    year,
+    month,
+    day,
+    asset,
+    tonnes,
+    product
+    destinationAsset
+  } = originalObj
+
+  console.log(year)  // 2021
+  console.log(month)  // 1
+  // ... other variables will have the corresponding values
+  `;
+
+  destructuringOperatorPickingValue: string = `
+  const obj = {
+    year; 2021, 
+    month: 1,
+    day: 31,
+    tonnes: 1000
+  }
+
+  // Only get the value of year and assign the rest to an object
+  const { year, ...theRest } = obj
+  
+  console.log(year)  // 2021
+  console.log(theRest)  // { month: 1, day: 31, tonnes: 1000 }, year has now been removed!
+
+
+  // Get the value of tonnes and assign it to another variable called 'ofrTonnes'
+  const { 
+    tonnes: ofrTonnes, 
+    year, 
+    month, 
+    day } = obj 
+  
+  console.log(ofrTonnes)  // 1000
+  `;
+
+  destructuringOperatorWritingFunction: string = `
+  interface TonnageValue {
+    year: number
+    month: number
+    asset: string
+    tonnes: number
+  }
+
+  // The 'traditional' way of writing a function that accept a TonnageValue object
+  function processTonnageValue(tonnageVal: TonnageValue) {
+    const year = tonnageVal.year
+    const month = tonnageVal.month
+    const asset = tonnageVal.asset
+    const tonnes = tonnageVal.tonnes
+
+    // ... Do other calculations
+  }
+
+  
+  // That same function but improved a bit
+  function processTonnageValueImproved(tonnageVal: TonnageValue) {
+    const { year, month, asset, tonnes } = tonnageVal
+    
+    // ... Do other calculations
+  }
+
+
+  // Let's bring it to the ultimate form!
+  function processTonnageValueImprovedVer2({ year, month, asset, tonnes }: TonnageValue) {
+    // ... Do other calculations
+  }
+  `;
+
+  ternaryOperatorExample: string = `
+  const age = 26
+  
+  // This is the long way of assigning proper value to beverage by checking
+  // for age
+  let beverage = ''
+  if (age < 18) {
+    beverage = 'juice'
+  }
+  else {
+    beverage = 'beer'
+  }
+
+  // Using ternary operator is a lot faster
+  let beverage = age < 18 ? 'juice' : 'beer'
+  `;
+
+  orOperatorExample: string = `
+  const userA = {
+    username: 'Peter Parker',
+    age: 26
+  }
+
+  const userB = {
+    username: '',
+    age: 20
+  }
+
+  // Use ternary operator
+  const nameA = userA.username ? userA.username : 'Anonymous'
+  const nameB = userB.username ? userB.username : 'Anonymous'
+
+
+  // Or we can use OR operator
+  const nameA = userA.username || 'Anonymous'
+  const nameB = userB.username || 'Anonymous'
+
+  console.log(nameA)  // 'Peter Parker'
+  console.log(nameB)  // 'Anonymous' because empty string is falsy
+  `;
+
+  nullishCoalescingExample: string = `
+  const userA = {
+    username: '',
+    age: 26
+  }
+
+  const userB = {
+    age: 20
+  }
+
+  const nameA = userA.username ?? 'Anonymous'
+  const nameB = userB.username ?? 'Anonymous'
+
+  console.log(nameA)   // ''
+  console.log(nameB)   // 'Anonymous' since username is undefined in userB
+  `;
+
+  chainingOperatorNaiveExample: string = `
+  const john = {
+    name: 'John',
+    cat: {
+      name: 'Tom',
+      meow: () => {
+        console.log('Meow!')
+      }
+    }
+  }
+
+  const marry = {
+    name: 'Marry',
+    dog: {
+      name: 'Chloe',
+      bark: () => {
+        console.log('Goof!')
+      }
+    }
+  }
+
+  // Since marry might or might not have a dog
+  // we must check if she rally has a dog before letting him/her bark
+  if (marry.dog) {
+    if (marry.dog.bark) {
+      marry.dog.bark()
+    }
+  }
+  `;
+
+  chainOperatorProblemExample: string = `
+  const marry = {
+    name: 'Marry',
+    dog: {
+      name: 'Chloe',
+      puppy: {
+        bark: () => {
+          console.log('Goof!)
+        }
+      }
+    }
+  }
+
+  // We check if Marry's dog has a puppy
+  // if yes, we let it bark
+  if (marry.dog) {
+    if (marry.dog.puppy) {
+      if (marry.dog.puppy.bark) {
+        marry.dog.puppy.bark()
+      }
+    }
+  }
+  `;
+
+  chainOperatorFixProblem: string = `
+  const marry = {
+    name: 'Marry',
+    dog: {
+      name: 'Chloe',
+      puppy: {
+        bark: () => {
+          console.log('Goof!)
+        }
+      }
+    }
+  }
+
+  // This will print to the console if Marry really has a dog, which also has a 
+  marry.dog?.puppy?.bark?.()
+  `;
+
   constructor() {}
 
   ngOnInit(): void {}
