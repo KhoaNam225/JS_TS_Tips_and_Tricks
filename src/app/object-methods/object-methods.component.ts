a09ec20e372f02edae10fb5903615e6f63d1b390
KhoaNam225 <pknam25@gmail.com>
2022-02-15 14:46:06 +0800

    Added content for Object methods

--- src/app/object-methods/object-methods.component.ts
+++ src/app/object-methods/object-methods.component.ts
@@ -3,13 +3,110 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-object-methods',
   templateUrl: './object-methods.component.html',
-  styleUrls: ['./object-methods.component.scss']
+  styleUrls: ['./object-methods.component.scss'],
 })
 export class ObjectMethodsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  objectOverview: string = `
+  const dog = {
+    legs: 4,
+    ears: 2,
+    eyes: 2,
+    name: 'John'
   }
 
+  console.log(dog.name)  // 'John'
+  console.log(dog['legs'])  // 4
+  `;
+
+  objectArrayStringOverview: string = `
+    const seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+    
+    console.log(seasons[0])   // 'Spring'
+    console.log(season['0'])  // Still 'Spring'! No ERROR!
+
+    const username = 'John Do'
+    console.log(username[0])  // 'J'
+    console.log(username['0'])  // Stil 'J'! No ERROR!
+  `;
+
+  objectKeysExample = `
+  const customerAge = {
+    'John': 18,
+    'Marry': 20,
+    'Chloe': 35
+  }
+
+  const customersName = Object.keys(customerAge)   // ['John', 'Marry', 'Chloe']
+
+  const customersUnder30 = customersName.filter(name => customerAge[name] < 30)
+
+  console.log(customerUnder30)  // ['John', 'Marry']
+  `;
+
+  objectValuesExample: string = `
+  const customerAge = {
+    'John': 18,
+    'Marry': 20,
+    'Chloe': 35
+  }
+
+  let customerUnder30Count = 0
+  Object.values(customerAge).forEach(age => {
+    if (age < 30) {
+      customerUnder30Count++
+    }
+  })
+
+  console.log(customerUnder30Count)  // 2
+  `;
+
+  objectEntriesExample: string = `
+  const customerAge = {
+    'John': 18,
+    'Marry': 20,
+    'Chloe': 35
+  }
+
+  Object.entries(customerAge).forEach(customer => {
+    const [name, age] = customer
+    console.log(name + ' is ' + age + ' years old!')
+  })
+
+  // John is 18 years old
+  // Marry is 20 years old
+  // Chloe is 35 years old
+  `;
+
+  objectAssignExample: string = `
+  const target = {}
+
+  const person1 = {
+    name: 'John',
+    age: 25,
+    sex: 'M'
+  }
+
+  const person2 = {
+    name: 'Marry',
+    age: 30,
+    sex: 'F'
+  }
+
+  Object.assign(target, person1)
+
+  console.log(target)  // { name: 'John', age: 25, sex: 'M' }
+
+  Object.assign(target, person2)
+
+  console.log(target)  // { name: 'Marry', age: 30, sex: 'F' }
+
+  // Object.assign() also accepts more than 2 arguments
+  // The first argument is always the target, then the subsequent arguments will be 'paste' onto the target one by one from left to right
+  Object.assign(target, person1, person2)
+
+  console.log(target)  // { name: 'Marry', age: 30, sex: 'F' }
+  `;
+  constructor() {}
+
+  ngOnInit(): void {}
 }
