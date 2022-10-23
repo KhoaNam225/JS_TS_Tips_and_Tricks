88e289fa9aa6c252fbda152e746bf565c0e06013
Nam Pham <pknam25@gmail.com>
2022-10-23 20:43:34 +0800

    Added API key to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Authentication information for Judge0
+  rapidAPIKey: '99112f997fmsh1ba9b63dbe0a090p19400fjsnf74dc14ba4c8',
+  rapidAPIHost: 'judge0-ce.p.rapidapi.com',
+  rapidAPIBaseURL: 'https://judge0-ce.p.rapidapi.com',
 };
